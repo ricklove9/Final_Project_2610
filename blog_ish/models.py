ce8041c1a2485d5f5a58d4e4b05e9c1acc1d72bc
Richard Loveland <ricky.loveland96@gmail.com>
2017-04-28 02:00:13 +0000

    deleted unused user model

--- blog_ish/models.py
+++ blog_ish/models.py
@@ -2,20 +2,6 @@ from __future__ import unicode_literals
 from datetime import datetime
 from django.db import models
 
-class UserManager(models.Manager):
-    def create_user(self, username):
-        user = self.create(name_text=username)
-        return user
-
-class User(models.Model):
-    name_text = models.CharField(max_length= 50, primary_key=True)
-    
-    objects = UserManager()
-    
-    def __str__(self):
-        return self.name_text
-        
-
 class PostManager(models.Manager):
     def create_post(self, post_text):
         post = self.create(post_text=post_text, date_published=datetime.now())
